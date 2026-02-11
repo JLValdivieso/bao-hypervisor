247bf5c32d149c64e3ffd4f901664ff1d0ba96a3
David Cerdeira <davidmcerdeira@gmail.com>
2026-02-11 13:11:27 +0000

    ref(rh850): don't emul intc1
    Signed-off-by: David Cerdeira <davidmcerdeira@gmail.com>

--- src/arch/rh850/vintc.c
+++ src/arch/rh850/vintc.c
@@ -35,16 +35,8 @@ static void emulate_intc_eic_access(struct emul_access* acc, size_t reg_idx, uns
     struct vm* vm = vcpu->vm;
 
     size_t addr_off = acc->addr & 0x1UL;
-    irqid_t int_id = 0;
-    volatile uint16_t* tgt_reg = NULL;
-
-    if (acc->addr < platform.arch.intc.intc1_addr) { /* INTC2 */
-        tgt_reg = &(intc2_hw->EIC[reg_idx]);
-        int_id = reg_idx + 32;
-    } else {                                         /* INTC1 */
-        int_id = reg_idx;
-        tgt_reg = &(intc1_hw->EIC[reg_idx]);
-    }
+    volatile uint16_t* tgt_reg = &(intc2_hw->EIC[reg_idx]);
+    irqid_t int_id = reg_idx + 32;
 
     if (!vm_has_interrupt(vm, int_id)) {
         ERROR("VM tried to access unassigned interrupt");
@@ -76,18 +68,11 @@ static void emulate_intc_imr_access(struct emul_access* acc, size_t reg_idx, uin
     struct vcpu* vcpu = cpu()->vcpu;
     struct vm* vm = vcpu->vm;
     size_t addr_off = acc->addr & 0x3UL;
-    irqid_t int_id = 0;
     irqid_t first_imr_int = 0;
-    volatile uint32_t* tgt_reg = NULL;
+    volatile uint32_t* tgt_reg = &(intc2_hw->IMR[reg_idx]);
 
-    if (acc->addr < platform.arch.intc.intc1_addr) { /* INTC2 */
-        first_imr_int = reg_idx * 32;
-        tgt_reg = &(intc2_hw->IMR[reg_idx]);
-        first_imr_int += 32;
-    } else { /* INTC1 */
-        first_imr_int = 0;
-        tgt_reg = &(intc1_hw->IMR);
-    }
+    first_imr_int = reg_idx * 32;
+    first_imr_int += 32;
 
     /* bit manipulation instruction */
     if (acc->arch.op != EMUL_ARCH_BWOP_NO) {
@@ -99,7 +84,7 @@ static void emulate_intc_imr_access(struct emul_access* acc, size_t reg_idx, uin
         unsigned long write_val = *tgt_reg;
 
         for (unsigned int i = first_imr_int; i < first_imr_int + 32; i++) {
-            if (!vm_has_interrupt(vm, i) || (int_id == IPI_HYP_IRQ_ID)) {
+            if (!vm_has_interrupt(vm, i) || (i == IPI_HYP_IRQ_ID)) {
                 continue;
             }
 
@@ -115,7 +100,7 @@ static void emulate_intc_imr_access(struct emul_access* acc, size_t reg_idx, uin
         unsigned long val = 0;
 
         for (unsigned int i = first_imr_int; i < first_imr_int + 32; i++) {
-            if (!vm_has_interrupt(vm, i) || (int_id == IPI_HYP_IRQ_ID)) {
+            if (!vm_has_interrupt(vm, i) || (i == IPI_HYP_IRQ_ID)) {
                 continue;
             }
 
@@ -141,15 +126,8 @@ static void emulate_intc_eibd_access(struct emul_access* acc, size_t reg_idx, ui
 
     size_t addr_off = acc->addr & 0x3UL;
     irqid_t int_id = 0;
-    volatile uint32_t* tgt_reg = NULL;
-
-    if (acc->addr < platform.arch.intc.intc1_addr) { /* INTC2 */
-        tgt_reg = &(intc2_hw->EIBD[reg_idx]);
-        int_id = reg_idx + 32;
-    } else {                                         /* INTC1 */
-        int_id = reg_idx;
-        tgt_reg = &(intc1_hw->EIBD[reg_idx]);
-    }
+    volatile uint32_t* tgt_reg = &(intc2_hw->EIBD[reg_idx]);
+    int_id = reg_idx + 32;
 
     if (!vm_has_interrupt(vm, int_id)) {
         ERROR("VM tried to access unassigned interrupt");
@@ -195,54 +173,14 @@ static void emulate_intc_eibd_access(struct emul_access* acc, size_t reg_idx, ui
     }
 }
 
-static void emulate_intc_fibd_access(struct emul_access* acc, uint32_t mask)
-{
-    struct vcpu* vcpu = cpu()->vcpu;
-    struct vm* vm = vcpu->vm;
-
-    size_t addr_off = acc->addr & 0x3UL;
-    volatile uint32_t* tgt_reg = &(intc1_hw->FIBD);
-
-    if (acc->arch.op != EMUL_ARCH_BWOP_NO || acc->write) {
-        /* FIBD register can not be written/modified by any guest */
-    } else {
-        unsigned long val = 0;
-        unsigned long phys_peid = *tgt_reg & 0x7UL;
-        unsigned long virt_peid = INVALID_CPUID;
-        for (size_t i = 0; i < vm->cpu_num; i++) {
-            if (vm->vcpus[i].phys_id == phys_peid) {
-                virt_peid = vm->vcpus[i].id;
-            }
-        }
-        if (virt_peid != INVALID_CPUID) {
-            val = virt_peid & 0x7UL;
-        }
-
-        val = (val >> (addr_off * 8)) & mask;
-        if (acc->sign_ext && (1UL << ((acc->reg_width * 8) + 7) & val)) {
-            val |= ~mask;
-        }
-
-        vcpu_writereg(vcpu, acc->reg, val);
-    }
-}
-
 static void emulate_intc_eeic_access(struct emul_access* acc, size_t reg_idx, uint32_t mask)
 {
     struct vcpu* vcpu = cpu()->vcpu;
     struct vm* vm = vcpu->vm;
 
     size_t addr_off = acc->addr & 0x3UL;
-    irqid_t int_id = 0;
-    volatile uint32_t* tgt_reg = NULL;
-
-    if (acc->addr < platform.arch.intc.intc1_addr) { /* INTC2 */
-        tgt_reg = &(intc2_hw->EEIC[reg_idx]);
-        int_id = reg_idx + 32;
-    } else {                                         /* INTC1 */
-        int_id = reg_idx;
-        tgt_reg = &(intc1_hw->EEIC[reg_idx]);
-    }
+    volatile uint32_t* tgt_reg = &(intc2_hw->EEIC[reg_idx]);
+    irqid_t int_id = reg_idx + 32;
 
     if (!vm_has_interrupt(vm, int_id)) {
         ERROR("VM tried to access unassigned interrupt");
@@ -266,62 +204,6 @@ static void emulate_intc_eeic_access(struct emul_access* acc, size_t reg_idx, ui
 
         vcpu_writereg(vcpu, acc->reg, val);
     }
-}
-
-static bool vintc1_emul_handler(struct emul_access* acc)
-{
-    size_t acc_offset = acc->addr - platform.arch.intc.intc1_addr;
-    unsigned long mask =
-        ((1U << (8 * (acc->reg_width + 1))) - 1) | ((acc->reg_width == 2) * 0xFF000000);
-
-    size_t intc1_eic_bot = offsetof(struct intc1, EIC);
-    size_t intc1_eic_top = sizeof(((struct intc1*)NULL)->EIC) + intc1_eic_bot;
-    size_t intc1_eic_idx = (ALIGN(acc_offset - intc1_eic_bot, 2)) / 2;
-    if (acc_offset >= intc1_eic_bot && acc_offset < intc1_eic_top &&
-        intc1_eic_idx != IPI_HYP_IRQ_ID) {
-        emulate_intc_eic_access(acc, intc1_eic_idx, mask);
-        return true;
-    }
-
-    size_t intc1_imr_bot = offsetof(struct intc1, IMR);
-    size_t intc1_imr_top = sizeof(((struct intc1*)NULL)->IMR) + intc1_imr_bot;
-    size_t intc1_imr_idx = (ALIGN(acc_offset - intc1_imr_bot, 4)) / 4;
-    if (acc_offset >= intc1_imr_bot && acc_offset < intc1_imr_top) {
-        emulate_intc_imr_access(acc, intc1_imr_idx, mask);
-        return true;
-    }
-
-    size_t intc1_eibd_bot = offsetof(struct intc1, EIBD);
-    size_t intc1_eibd_top = sizeof(((struct intc1*)NULL)->EIBD) + intc1_eibd_bot;
-    size_t intc1_eibd_idx = (ALIGN(acc_offset - intc1_eibd_bot, 4)) / 4;
-    if (acc_offset >= intc1_eibd_bot && acc_offset < intc1_eibd_top &&
-        intc1_eibd_idx != IPI_HYP_IRQ_ID) {
-        emulate_intc_eibd_access(acc, intc1_eibd_idx, mask);
-        return true;
-    }
-
-    size_t intc1_fibd_bot = offsetof(struct intc1, FIBD);
-    size_t intc1_fibd_top = sizeof(((struct intc1*)NULL)->FIBD) + intc1_fibd_bot;
-    if (acc_offset >= intc1_fibd_bot && acc_offset < intc1_fibd_top) {
-        emulate_intc_fibd_access(acc, mask);
-        return true;
-    }
-
-    size_t intc1_eeic_bot = offsetof(struct intc1, EEIC);
-    size_t intc1_eeic_top = sizeof(((struct intc1*)NULL)->EEIC) + intc1_eeic_bot;
-    size_t intc1_eeic_idx = (ALIGN(acc_offset - intc1_eeic_bot, 4)) / 4;
-    if (acc_offset >= intc1_eeic_bot && acc_offset < intc1_eeic_top &&
-        intc1_eeic_idx != IPI_HYP_IRQ_ID) {
-        emulate_intc_eeic_access(acc, intc1_eeic_idx, mask);
-        return true;
-    }
-
-    /* Ignore access */
-    if (!acc->write && acc->arch.op == EMUL_ARCH_BWOP_NO) {
-        vcpu_writereg(cpu()->vcpu, acc->reg, 0);
-    }
-
-    return true;
 }
 
 static bool vintc2_emul_handler(struct emul_access* acc)
@@ -401,13 +283,6 @@ static bool vfeinc_emul_handler(struct emul_access* acc)
 void vintc_init(struct vm* vm)
 {
     if (cpu()->id == vm->master) {
-        vm->arch.intc1_emul = (struct emul_mem){
-            .va_base = platform.arch.intc.intc1_addr,
-            .size = ALIGN(sizeof(struct intc1), PAGE_SIZE),
-            .handler = vintc1_emul_handler,
-        };
-        vm_emul_add_mem(vm, &vm->arch.intc1_emul);
-
         vm->arch.intc2_emul = (struct emul_mem){
             .va_base = platform.arch.intc.intc2_addr,
             .size = ALIGN(sizeof(struct intc2), PAGE_SIZE),
@@ -455,7 +330,6 @@ void vintc_vcpu_reset(struct vcpu* vcpu)
             intc_set_trgt(i, vcpu->phys_id);
             intc_set_enable(i, false);
             intc_set_prio(i, 0);
-            // intc_set_act(i, false);
             intc_set_pend(i, false);
         }
     }
@@ -469,7 +343,6 @@ void vintc_vm_reset(struct vm* vm)
                 intc_set_trgt(i, cpu()->id);
                 intc_set_enable(i, false);
                 intc_set_prio(i, 0);
-                // intc_set_act(i, false);
                 intc_set_pend(i, false);
             }
         }
